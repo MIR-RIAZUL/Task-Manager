76f054ab9485790840921dd7e4b540e2b9470589
MIR-RIAZUL <mirriazul859@gmail.com>
2026-09-05 04:46:36 +0600

    without the waiting time

--- lib/ui/screens/splash_screen.dart
+++ lib/ui/screens/splash_screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
-import 'package:task_manager/ui/screens/login_screen.dart';
 import 'package:task_manager/ui/utils/asset_paths.dart';
 
 class SplashScreen extends StatefulWidget {
@@ -11,22 +10,6 @@ class SplashScreen extends StatefulWidget {
 }
 
 class _SplashScreenState extends State<SplashScreen> {
-  @override
-  void initState() {
-    super.initState();
-    _moveToNextScreen();
-  }
-
-  Future<void> _moveToNextScreen() async {
-    await Future.delayed(const Duration(seconds: 3));
-    if (mounted) {
-      Navigator.pushReplacement(
-        context,
-        MaterialPageRoute(builder: (_) => const LoginScreen()),
-      );
-    }
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
