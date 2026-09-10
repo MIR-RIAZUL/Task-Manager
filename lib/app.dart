a5d715debd7fd9cfd3c6e3012e608f5a7abc1908
Nexus-Portal <dev@nexus.com globally>
2026-09-10 23:56:22 +0600

    theme data

--- lib/app.dart
+++ lib/app.dart
@@ -7,9 +7,19 @@ class Task_manager extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(
+        inputDecorationTheme: InputDecorationTheme(
+          hintStyle: TextStyle(color: Colors.grey),
+          fillColor: Colors.white,
+          filled: true,
+          contentPadding: EdgeInsets.symmetric(horizontal: 16),
+          border: OutlineInputBorder(borderSide: BorderSide.none),
+          enabledBorder: OutlineInputBorder(borderSide: BorderSide.none),
+          errorBorder: OutlineInputBorder(borderSide: BorderSide.none),
+        ),
+      ),
       debugShowCheckedModeBanner: false,
-      home:  SplashScreen(),
-
+      home: SplashScreen(),
     );
   }
 }
