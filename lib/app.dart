e02bb542792e420ba895c9350c447bd611f879e3
Nexus-Portal <dev@nexus.com globally>
2026-09-11 03:05:44 +0600

    add forgot password and reset password screens; update login and app theme,sign in screen; update verify,verify otp screens;

--- lib/app.dart
+++ lib/app.dart
@@ -8,14 +8,29 @@ class Task_manager extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(
+        textTheme: TextTheme(
+          titleLarge: TextStyle(fontSize: 30, fontWeight: FontWeight.w600),
+        ),
+        colorSchemeSeed: Colors.grey,
         inputDecorationTheme: InputDecorationTheme(
-          hintStyle: TextStyle(color: Colors.grey),
+          hintStyle: TextStyle(color: Colors.green),
+
           fillColor: Colors.white,
           filled: true,
-          contentPadding: EdgeInsets.symmetric(horizontal: 16),
+          contentPadding: EdgeInsets.symmetric(horizontal: 16, vertical: 14),
           border: OutlineInputBorder(borderSide: BorderSide.none),
           enabledBorder: OutlineInputBorder(borderSide: BorderSide.none),
           errorBorder: OutlineInputBorder(borderSide: BorderSide.none),
+        ),
+        filledButtonTheme: FilledButtonThemeData(
+          style: FilledButton.styleFrom(
+            fixedSize: Size.fromWidth(double.maxFinite),
+            padding: EdgeInsets.symmetric(horizontal: 14, vertical: 16),
+            backgroundColor: Colors.green,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(15),
+            ),
+          ),
         ),
       ),
       debugShowCheckedModeBanner: false,
