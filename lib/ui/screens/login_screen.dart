a4319ee8cadbad8ca6953a20ce40fdca8a204791
MIR RIAZUL <mirriazul859@gmail.com>
2026-09-10 23:56:22 +0600

    theme data

--- lib/ui/screens/login_screen.dart
+++ lib/ui/screens/login_screen.dart
@@ -8,29 +8,46 @@ class LoginScreen extends StatefulWidget {
   State<LoginScreen> createState() => _LoginScreenState();
 }
 
+final TextEditingController emailController = TextEditingController();
+final TextEditingController passwordController = TextEditingController();
+
 class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: ScreenBackground(
-        child: Column(
-          children: [
-            Text('Login'),
-            TextFormField(),
-            TextFormField(),
-            FilledButton(onPressed: () {}, child: Icon(Icons.login)),
-            TextButton(onPressed: () {}, child: Text('forget password')),
-            RichText(
-              text: TextSpan(
-                style: TextStyle(
-                  color: Colors.blue,
-                  decoration: TextDecoration.underline,
-                ),
-                text: "Don't have account? ",
-                children: [TextSpan(text: "Sign Up")],
+        child: Padding(
+          padding: const EdgeInsets.all(20),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const SizedBox(height: 100),
+              Text(
+                'Get Start With',
+                style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
               ),
-            ),
-          ],
+              const SizedBox(height: 20),
+              TextFormField(decoration: InputDecoration(hintText: 'Email')),
+              const SizedBox(height: 20),
+              TextFormField(
+                obscureText: true,
+                decoration: InputDecoration(hintText: 'Password'),
+              ),
+              const SizedBox(height: 20),
+              FilledButton(onPressed: () {}, child: Icon(Icons.login)),
+              TextButton(onPressed: () {}, child: Text('forget password')),
+              RichText(
+                text: TextSpan(
+                  style: TextStyle(
+                    color: Colors.blue,
+                    decoration: TextDecoration.underline,
+                  ),
+                  text: "Don't have account? ",
+                  children: [TextSpan(text: "Sign Up")],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
--- lib/ui/screens/splash_screen.dart
+++ lib/ui/screens/splash_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:task_manager/ui/screens/login_screen.dart';
 import 'package:task_manager/ui/utils/asset_paths.dart';
+import 'package:task_manager/ui/widgets/screen_backround.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
@@ -30,16 +31,8 @@ class _SplashScreenState extends State<SplashScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
-        children: [
-          SvgPicture.asset(
-            asset_paths.background,
-            width: double.maxFinite,
-            height: double.maxFinite,
-            fit: BoxFit.cover,
-          ),
-          Center(child: SvgPicture.asset(asset_paths.logo, height: 80)),
-        ],
+      body: ScreenBackground(
+        child: Center(child: SvgPicture.asset(asset_paths.logo, height: 80)),
       ),
     );
   }
